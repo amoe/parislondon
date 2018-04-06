1002c72f327aee52a5dde8f508fc17ab0149b118
David Banks <amoebae@gmail.com>
2018-04-06 14:12:35 +0100

    initial import

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+// webpack configuration file
+
+const webpack = require('webpack');
+
+module.exports = {
+    entry: ["./src/entry.ts"],
+    output: {
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                options: {
+                    appendTsSuffixTo: [/\.vue$/]
+                }
+            }
+        ],
+        loaders: [
+        ]
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        port: 57237,
+        proxy: {
+            "/api": {
+                target: "http://localhost:3000",
+                pathRewrite: {"^/api": ""}
+            }
+        },
+        // Force the dev server to serve the index XHTML page
+        historyApiFallback: {
+            index: 'index.xhtml'
+        }
+    }
+};
