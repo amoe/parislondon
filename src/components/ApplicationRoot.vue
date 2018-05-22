9ed263971133234273f4b53003327b730cf18cf5
David Banks <amoebae@gmail.com>
2018-05-22 11:41:11 +0100

    starting to transplant leaflet playground

--- src/components/ApplicationRoot.vue
+++ src/components/ApplicationRoot.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-    <h1>Paris-London; Global Music Connections 1962-89</h1>
+  <div class="content">
+    <div class="header">
+      Paris-London; Global Music Connections 1962-89
+    </div>
 
+    <div class="maps">
+      <div id="map-paris" class="map"></div>
+      <div id="map-london" class="map"></div>
+    </div>
+    <div class="sidebar">
+      <h1>PARIS / LONDON</h1>
 
-  <fry-bender :center="kilometreZero"></fry-bender>
-  <fry-bender :center="equestrianStatue"></fry-bender>
+      <h2>Population</h2>
 
-  {{locationData}}
-
+      <div id="chart"></div>
+    </div>
   </div>
 </template>
 
@@ -48,15 +55,79 @@ export default Vue.extend({
 </script>
 
 <style>
-body {
-    max-width: 64rem;
-    margin-left: auto;
-    margin-right: auto;
-    background-color: #fdfdfd;
+@font-face {
+    font-family: 'Oxygen';
+    src: url("static/fonts/Oxygen-Regular.ttf");
 }
 
-h1,h2 { font-family: Georgia; }
+@font-face {
+    font-family: 'Vollkorn';
+    src: url("static/fonts/Vollkorn-Regular.ttf");
+}
 
-p, label { font-family: Arial, sans-serif; }
+
+div.content {
+    display: grid;
+    grid-column-gap: 1rem;
+    grid-row-gap: 1rem;
+    grid-template-columns: repeat(12, 1fr);
+}
+
+
+/* This class applies to both maps */
+.map {
+    height: 46vh;
+    width: 81vw;
+    border: 1px solid red;
+}
+
+.maps {
+    grid-row-start: 2;
+    grid-column-start: span 10;
+    border: 1px solid blue;
+}
+
+.header {
+    grid-row-start: 1;
+    grid-column-start: span 12;
+    font-family: 'Vollkorn', serif;
+    font-size: x-large;
+    margin-top: 0.4em;
+    margin-left: 0.4em;
+}
+
+.sidebar {
+    grid-row-start: 2;
+    grid-column-start: span 2;
+    border: 1px solid green;
+}
+
+
+h1 {
+    font-family: 'Vollkorn', serif;
+    text-align: center;
+}
+
+h2 {
+    font-family: 'Oxygen', sans-serif;
+    text-align: center;
+}
+
+
+/* Hide the leaflet attribution */
+.leaflet-control-attribution { display: none; }
+
+#chart {
+    margin-right: 1em;
+}
+
+g.tick {
+    font-family: 'Oxygen', sans-serif;
+}
+
+g.c3-legend-item {
+    font-family: 'Oxygen', sans-serif;
+}
+
 
 </style>
