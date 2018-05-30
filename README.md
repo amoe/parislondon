82e47b48fc9a240d52ceb74de973b3aeb1d643fb
David Banks <amoebae@gmail.com>
2018-05-30 19:19:32 +0100

    trying to use shipit for deployment

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ The key needs to be provided from the outside file `~/.apikey.json`.
     {
         "GOOGLE_MAPS_API_KEY": "AIzaSyBqfBtliy4zL76AcW22ESgXA3CyPk9orpE"
     }
+
+
+## Build & Deploy
+
+Build: `make -f main.mk dist`
+
+Deploy: `npx shipit staging deploy`
--- shipitfile.js
+++ shipitfile.js
@@ -5,11 +5,26 @@ const shipitDeploy = require('shipit-deploy');
 module.exports = shipit => {
     shipitDeploy(shipit);
 
+    shipit.blTask('build', () => {
+        console.log('wtf');
+        const dir = shipit.releasePath;
+        console.log("dir is %o", dir);
+
+        return shipit.local("echo " + dir);
+    });
+
+
+    // the way that shipit works, you need to handle 'fetched' event, created by
+    // 'deploy' and build the output, and probably also setting 'dirToCopy'
+    shipit.on('update', () => {
+        return shipit.start('build');
+    });
 
     shipit.initConfig({
         default: {
             deployTo: '/srv/http/solasistim/shl/parislondon',
             repositoryUrl: 'https://github.com/amoe/parislondon.git',
+            dirToCopy: 'dist'
         },
         staging: {
             servers: 'amoe@visarend.solasistim.net',
